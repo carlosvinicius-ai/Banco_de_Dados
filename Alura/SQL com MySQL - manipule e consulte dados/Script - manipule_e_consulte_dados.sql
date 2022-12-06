b2758a25a0d576e8a73862427fd7d15a1d0f5bb1
cvinicius-ai <cvinicius32@hotmail.com>
2022-12-05 22:13:59 -0300

    adicionando informacoes ao banco de dados

--- Alura/SQL com MySQL - manipule e consulte dados/Script - manipule_e_consulte_dados.sql
+++ Alura/SQL com MySQL - manipule e consulte dados/Script - manipule_e_consulte_dados.sql
@@ -70,6 +70,23 @@ values (
     0.10
 );
 
+insert into tb_produto (
+ID_PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) values (
+'1037797', 'Clean - 2 Litros - Laranja',
+'PET', '2 Litros', 'Laranja', 16.01);
+insert into tb_produto (
+ID_PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) values (
+'1000889', 'Sabor da Montanha - 700 ml - Uva',
+'Garrafa', '700 ml', 'Uva', 6.31);
+insert into tb_produto (
+ID_PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) values (
+'1004327', 'Videira do Campo - 1,5 Litros - Melância',
+'PET', '1,5 Litros', 'Melância', 19.51);
+
 -- Para visualizar os dados que foram adicionados
 
 select * from tb_produto;
+
